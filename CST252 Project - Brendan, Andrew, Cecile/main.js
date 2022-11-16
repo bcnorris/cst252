ba159c8d219f965a84ee9ce94320ab379cf64d9f
bcnorris <brendannorriscd18@gmail.com>
2022-11-16 15:53:54 -0800

    Added for loop to mark each square

--- CST252 Project - Brendan, Andrew, Cecile/main.js
+++ CST252 Project - Brendan, Andrew, Cecile/main.js
@@ -1,28 +1,39 @@
+
+//Making the Next Button generate a new random number
+let nextButton = document.querySelector(".nextButton");
+  console.log(nextButton);
+
+let clickNext = () => {
+  document.querySelector(".nextNumber").textContent = Math.floor(Math.random() * 100);
+}
+
+nextButton.addEventListener('click', clickNext);
+
+
+
 //For loop filling bingo sqaures 1-25 (0-24)
 for (let i = 0; i < 25; i++) {
     document.getElementById("square" + i).innerHTML = Math.floor(Math.random() * 100);
   }
   //setting sqaure 13 (12) as a free space
   document.getElementById("square" + 12).innerHTML = "Free";
+
+
+  let state = 'empty';
+
+  let selectedSquare = '';
   
-  //anon function to change bg color when a space is selected by user
-  let squareFill = () => {
-    document.querySelector('#square0').style.backgroundColor = "green";
-    document.querySelector('#square0').style.color = "white";
-  }
-  //listner for on click of square 0 
-  document.querySelector('#square0').addEventListener("click", squareFill);
-  //anon function to change bg color when a space is selected by user
-  let squareFill1 = () => {
-    document.querySelector('#square1').style.backgroundColor = "green";
-    document.querySelector('#square1').style.color = "white";
-  }
-  //listner for on click of square 1
-  document.querySelector('#square1').addEventListener("click", squareFill1);
-  //anon function to change bg color when a space is selected by user
-  let squareFill2 = () => {
-    document.querySelector('#square2').style.backgroundColor = "green";
-    document.querySelector('#square2').style.color = "white";
-  }
-  //listner for on click of square 2 
-  document.querySelector('#square2').addEventListener("click", squareFill2);
+  let clickSquare = (e) => {
+
+		state = 'selected';
+
+		let winSquare = e.currentTarget;
+
+    winSquare.classList.add("markedSquare");
+
+  };
+
+  let winSquares = document.querySelectorAll(".square");
+  winSquares.forEach(winSquare => {
+		winSquare.addEventListener('click', clickSquare);
+	});
